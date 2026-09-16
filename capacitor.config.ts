16255d686bd6b25129e011c54233c7ae15bb4f5f
solomonufo77-bit <solomonufo77@gmail.com>
2026-09-16 18:25:58 +0100

    Update appId in capacitor.config.ts

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.AgeJoy.app',
+  appId: 'com.AgeJoy.myapp',
   appName: 'AgeJoy',
   webDir: 'dist',
   bundledWebRuntime: false,
